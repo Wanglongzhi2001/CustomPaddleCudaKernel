3b4af9212dc3d039478eb628f096e9033e3e30dd
Wanglongzhi2001 <583087864@qq.com>
2024-04-25 23:27:12 +0800

    add benchmark

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # CustomTorchCudaKernel
 实现自定义PyTorch的CUDA算子，用来学习CUDA以及benchmark和profile
+
+# Usage
+## build
+'''
+cd pytorch
+mkdir build && cd build
+cmake ..
+make -j$(nproc)
+'''
+## run benchmark
+'''
+cd ../benchmark
+python benchmark.py
+'''
---
+++ pytorch/benchmark/benchmark.py
@@ -0,0 +1,37 @@
+import torch
+import time
+
+LIB_PATH = "../build/libmy_add.so"
+
+if __name__ == '__main__':
+    torch.ops.load_library(LIB_PATH)
+
+    # init input
+    n = 1024
+    a = torch.rand([1,n]).cuda()
+    b = torch.rand([1,n]).cuda()
+    c = torch.empty(n).to(device="cuda:0")
+    warmup_times = 10
+    repeat_times = 100
+    # warmup
+    for _ in range(warmup_times):
+        torch_res = torch.add(a, b)
+    # run torch kernel
+    tic = time.perf_counter()
+    for _ in range(repeat_times):
+        torch_res = torch.add(a, b)
+    toc = time.perf_counter()
+    print(f"run torch kernel cost: {(toc - tic) / repeat_times}")
+
+
+    # warmup
+    for _ in range(warmup_times):
+        torch_res = torch.add(a, b)
+    # run torch kernel
+    tic = time.perf_counter()
+    for _ in range(repeat_times):
+        torch.ops.my_add.torch_launch_my_add(c, a, b, n)
+    toc = time.perf_counter()
+    print(f"run custom kernel cost: {(toc - tic) / repeat_times}")
+
+
